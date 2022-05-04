bf81b6b78e2dedcae8a2f9c413b0a54d412e2f60
math0898 <25396616+math0898@users.noreply.github.com>
2022-05-04 12:47:22 -0500

    Too Fancy
    - Added a main to run the algorithm on a requested input.
    
    - Added Jest with some tests to run on the algorithm.

--- 4. Median of Two Sorted Arrays/main.js
+++ 4. Median of Two Sorted Arrays/main.js
@@ -1,2 +1,16 @@
+const prompt = require('prompt-sync')();
+const findMedianSortedArrays = require('./medianSortedArrays');
 
-console.log('Hello world!');
+function sort (a, b) { return a - b } 
+var n = prompt('Array 1 size: ');
+var m = prompt('Array 2 size: ');
+var nums1 = [];
+var nums2 = [];
+
+for (var i = 0; i < n; i++) nums1.push(prompt('Next number in array 1: '));
+for (var i = 0; i < m; i++) nums2.push(prompt('Next number in array 2: '));
+
+nums1.sort(sort);
+nums2.sort(sort);
+ 
+console.log('The median of the two arrays is: ' + findMedianSortedArrays(nums1, nums2));
